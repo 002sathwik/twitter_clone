28bcb41af6cefd23772090e62bd456e16dee3604
Sathwik <hsathwik002@gmail.com>
2024-05-25 21:54:41 +0530

    postform/fech

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,11 @@
+export default function NewTweetForm() {
+  return (
+    <form className=" flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+        {/*  <ProfileImage src="#"/>*/}
+        <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="What's happening" />
+      </div>
+      <button>sasas</button>
+    </form>
+  );
+}
