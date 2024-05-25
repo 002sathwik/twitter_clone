2bf6c49addceb069c1bd397b5f61b8c7cbc4badc
Sathwik <hsathwik002@gmail.com>
2024-05-25 20:44:02 +0530

    sidebar/fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { GeistSans } from "geist/font/sans";
+
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
@@ -6,6 +6,8 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
+import Sidenav from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -13,9 +15,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <main className={GeistSans.className}>
-        <Component {...pageProps} />
-      </main>
+      <Head>
+        <title>Twitter_colne</title>
+        <meta name="description" content="this is a twiter colne " />
+      </Head>
+      <div className="container mx-auto flex sm:pr-4">
+        <Sidenav/>
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
